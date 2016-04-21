3e42105e0b695955d7de0d692ae1b116c33e63b9
Christopher Aue <mail@christopheraue.net>
2016-04-21 19:10:57 +0200

    improved comment

--- lib/atspi.rb
+++ lib/atspi.rb
@@ -2,10 +2,10 @@ require 'forwardable'
 require 'gir_ffi'
 
 # I would like to name the Atspi module as Libatspi but there seems to be no
-# easy way to namespace or rename modules imported by gir ffi. The Atspi name
+# easy way to namespace or rename modules imported by GirFFI. The Atspi name
 # has to stay b/c it seems to be expected when loading other classes and
-# calling methods. Therefore, Atspi are left as it is but is aliased as
-# Libatspi to better differentiate it from our ATSPI namespace
+# calling methods. Therefore, Atspi is left as it is but is aliased as
+# Libatspi to better differentiate it from our ATSPI namespace.
 GirFFI.setup :Atspi
 Libatspi = Atspi
 
