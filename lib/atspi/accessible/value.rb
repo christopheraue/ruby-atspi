96c4b2f21d83e2c17fdc12f455e8f578b9353ac2
Christopher Aue <mail@christopheraue.net>
2016-04-24 21:02:39 +0200

    documented Accessible::Value

--- lib/atspi/accessible/value.rb
+++ lib/atspi/accessible/value.rb
@@ -2,30 +2,44 @@ module ATSPI
   class Accessible::Value
     extend Forwardable
 
+    # @!visibility private
     def initialize(native)
       @native = native
     end
+    # @!visibility public
 
-    delegate %i(minimum_value maximum_value current_value) => :@native
-    delegate :set_current_value => :@native
-    delegate :minimum_increment => :@native
+    # @return [Float] its minimum
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-value.html#atspi-value-get-minimum-value atspi_value_get_minimum_value
+    def minimum
+      @native.minimum_value
+    end
 
-    alias_method :minimum, :minimum_value
-    alias_method :min, :minimum_value
+    # @return [Float] its maximum
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-value.html#atspi-value-get-maximum-value atspi_value_get_maximum_value
+    def maximum
+      @native.maximum_value
+    end
 
-    alias_method :maximum, :maximum_value
-    alias_method :max, :maximum_value
+    # @return [Float] its current value
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-value.html#atspi-value-get-current-value atspi_value_get_current_value
+    def current
+      @native.current_value
+    end
 
-    alias_method :current, :current_value
-    alias_method :cur, :current_value
-    alias_method :to_i, :current_value
+    # Sets its value to the given one.
+    # @return [true,false] indicating success
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-value.html#atspi-value-set-current-value atspi_value_set_current_value
+    def set_to(value)
+      @native.set_current_value(value)
+    end
 
-    alias_method :increment, :minimum_increment
-    alias_method :incr, :minimum_increment
-    alias_method :step, :minimum_increment
+    # @return [Float] its minimum increment
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-value.html#atspi-value-get-minimum-increment atspi_value_get_minimum_increment
+    def step
+      @native.minimum_increment
+    end
 
-    alias_method :set_to, :set_current_value
-
+    # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} @min=#{min} @cur=#{cur} @max=#{max} @incr=#{incr}>"
     end
