ad0db3ba7f4e7488f991ee3b8aaed1e07780708a
Christopher Aue <mail@christopheraue.net>
2016-04-24 20:45:51 +0200

    documented Desktop

--- lib/atspi/desktop.rb
+++ lib/atspi/desktop.rb
@@ -1,29 +1,52 @@
 module ATSPI
+  # Desktops are Accessibles having a few exceptions.
   class Desktop < Accessible
+  # @!group Identification
+    # It has no parent.
+    # @return [nil]
     def parent
       nil
     end
 
+    # It has no path.
+    # @return [[]]
     def path
       []
     end
 
+    # Its desktop is itself.
+    # @return [self]
     def desktop
       self
     end
 
+    # It belongs to no application.
+    # @return nil
     def application
       nil
     end
 
+    # It has no window.
+    # @return nil
     def window
       nil
     end
+  # @!endgroup
 
-    alias_method :applications, :children
+  # @!group Tree & Traversal
+    # Its applications are its children.
+    # @param (see Accessible#children)
+    # @return (see Accessible#children)
+    def applications
+      children
+    end
+  # @!endgroup
 
+  # @!group Representations
+    # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} @index=#{index} @name=#{name.inspect}>"
     end
+  # @!endgroup
   end
 end
