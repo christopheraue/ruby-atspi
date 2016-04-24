152326e27f776cd96ff7b04b0c958791d26c07ce
Christopher Aue <mail@christopheraue.net>
2016-04-24 21:16:34 +0200

    documented Accessible::Image

--- lib/atspi/accessible/image.rb
+++ lib/atspi/accessible/image.rb
@@ -1,21 +1,32 @@
 module ATSPI
   class Accessible::Image
+    # @!visibility private
     def initialize(native)
       @native = native
     end
+    # @!visibility public
 
+    # @return [String] its description
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-image.html#atspi-image-get-image-description atspi_image_get_image_description
     def description
       @native.image_description
     end
 
+    # @param (see Accessible::Extents#extents)
+    # @return [Extents] its extents
+    # @example (see Accessible::Extents#extents)
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-image.html#atspi-image-get-image-extents atspi_image_get_image_extents
     def extents(relative_to:)
       Extents.new(@native.image_extents(relative_to))
     end
 
+    # @return [String] its locale
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-image.html#atspi-image-get-image-locale atspi_image_get_image_locale
     def locale
       @native.image_locale
     end
 
+    # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} @description=#{description.inspect} " <<
         "@extents=#{extents(relative_to: :screen).inspect}>"
