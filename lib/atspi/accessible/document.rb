2a66f97e55c05e7b7cd56f8144ed6dc70dbb8a04
Christopher Aue <mail@christopheraue.net>
2016-04-24 21:21:17 +0200

    documented Accessible::Document

--- lib/atspi/accessible/document.rb
+++ lib/atspi/accessible/document.rb
@@ -1,21 +1,24 @@
 class ATSPI::Accessible
   class Document
+    # @!visibility private
     def initialize(native)
       @native = native
     end
+    # @!visibility public
 
+    # @return [String] its locale
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-document.html#atspi-document-get-locale atspi_document_get_locale
     def locale
       @native.locale
     end
 
-    def attribute_value(attribute)
-      @native.document_attribute_value(attribute.to_s)
-    end
-
+    # @return [Hash<String => String>] its attributes
+    # @see https://developer.gnome.org/libatspi/stable/libatspi-atspi-document.html#atspi-document-get-attributes atspi_document_get_attributes
     def attributes
       @native.document_attributes.to_h
     end
 
+    # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} #{attributes.map{ |k, v| "@#{k}=#{v.inspect}" }.join(' ')}>"
     end
