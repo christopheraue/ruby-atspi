c647ae2269750719c28e36d7351f7dda1abb770d
Christopher Aue <mail@christopheraue.net>
2016-04-24 20:46:02 +0200

    documented Application

--- lib/atspi/application.rb
+++ lib/atspi/application.rb
@@ -1,23 +1,46 @@
 module ATSPI
+  # Applications are Accessibles having a few exceptions.
   class Application < Accessible
+  # @!group Identification
+    # It has no path.
+    # @return [[]]
     def path
       []
     end
 
-    alias_method :desktop, :parent
+    # Its desktop is its parent
+    # @return [Desktop]
+    def desktop
+      parent
+    end
 
+    # Its application is itself
+    # @return [self]
     def application
       self
     end
 
+    # It belongs to no window
+    # @return nil
     def window
       nil
     end
+  # @!endgroup
 
-    alias_method :windows, :children
+  # @!group Tree & Traversal
+    # Its windows are its children.
+    # @param (see Accessible#children)
+    # @return (see Accessible#children)
+    def windows
+      children
+    end
+  # @!endgroup
 
+  # @!group Representations
+  # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} @desktop=#{desktop.index} @name=#{name.inspect}>"
     end
+  # @!endgroup
   end
 end
