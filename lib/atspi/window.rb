8de0ed46103497267244655dc6798160744a2997
Christopher Aue <mail@christopheraue.net>
2016-04-24 20:46:12 +0200

    documented Window

--- lib/atspi/window.rb
+++ lib/atspi/window.rb
@@ -1,19 +1,33 @@
 module ATSPI
+  # Windows are Accessibles having a few exceptions.
   class Window < Accessible
+  # @!group Identification
+    # It has no path.
+    # @return [[]]
     def path
       []
     end
 
-    alias_method :parent, :application
+    # Its parent is its application
+    # @return [Application]
+    def parent
+      application
+    end
 
+    # Its window is itself
+    # @return [self]
     def window
       self
     end
+  # @!endgroup
 
+  # @!group Representations
+  # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} @desktop=#{desktop.index} " <<
         "@application=#{application.name} @name=#{name.inspect} " <<
         "@extents=#{extents(relative_to: :screen).inspect}>"
     end
+  # @!endgroup
   end
 end
