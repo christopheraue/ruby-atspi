bf2b7775b2afb07f157f5875b03d4fe3f715e751
Christopher Aue <mail@christopheraue.net>
2016-04-25 23:16:51 +0200

    documented Extents

--- lib/atspi/extents.rb
+++ lib/atspi/extents.rb
@@ -1,15 +1,27 @@
 module ATSPI
+  # Wraps libatspi's AtspiRect[https://developer.gnome.org/libatspi/stable/libatspi-atspi-component.html#AtspiRect-struct]
   class Extents
     extend Forwardable
 
+    # @!visibility private
     def initialize(native)
       @native = native
     end
+    # @!visibility public
 
-    delegate %i(x y width height) => :@native
-    alias_method :left, :x
-    alias_method :top, :y
+    # @return [Integer] its x position
+    delegate :x => :@native
 
+    # @return [Integer] its y position
+    delegate :y => :@native
+
+    # @return [Integer] its width
+    delegate :width => :@native
+
+    # @return [Integer] its height
+    delegate :height => :@native
+
+    # @return [String] itself as an inspectable string
     def inspect
       "#<#{self.class.name}:0x#{'%x14' % __id__} @x=#{x} @y=#{y} @width=#{width} @height=#{height}>"
     end
